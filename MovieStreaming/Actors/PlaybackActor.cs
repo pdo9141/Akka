77c28f3246405e145bfa93e0165dd15980cad535
pdo9141 <pdo9141@gmail.com>
2016-04-30 11:53:48 -0700

    Initial checkin

---
+++ MovieStreaming/Actors/PlaybackActor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Akka.Actor;
+using MovieStreaming.Messages;
+
+namespace MovieStreaming.Actors
+{
+    public class PlaybackActor : ReceiveActor
+    {
+        public PlaybackActor()
+        {
+            Console.WriteLine("Creating a PlaybackActor");
+            Receive<PlayMovieMessage>(message => HandlePlayMovieMessage(message), message => message.UserId == 42);
+        }
+
+        private void HandlePlayMovieMessage(PlayMovieMessage message)
+        {
+            Console.WriteLine("Received movie title " + message.MovieTitle);
+            Console.WriteLine("Received user ID " + message.UserId);
+        }
+    }
+}
